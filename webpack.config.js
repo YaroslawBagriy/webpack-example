1e0a384f6efdae353d23f4ed2ece44062dbece64
yaroslawbagriy <yaroslawbagriy@gmail.com>
2020-06-26 08:15:29 -0500

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -15,7 +17,43 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { 
+                        loader: 'style-loader' 
+                    },
+                    { 
+                        loader: 'css-loader', 
+                        options: {
+                            importLoaders: 1,
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base]'
+                            }
+                        } 
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [autoprefixer()]
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif )$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
 }; 
